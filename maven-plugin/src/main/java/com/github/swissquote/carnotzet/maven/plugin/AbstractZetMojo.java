4465fa09213de48953bf36f4ceae85be7c71cb32
Manuel Ryan <ryan@shamu.ch>
2017-05-02 13:32:45 +0200

    Extract convenience method to run maven plugin from wrong folder

--- maven-plugin/src/main/java/com/github/swissquote/carnotzet/maven/plugin/AbstractZetMojo.java
+++ maven-plugin/src/main/java/com/github/swissquote/carnotzet/maven/plugin/AbstractZetMojo.java
@@ -1,6 +1,5 @@
 package com.github.swissquote.carnotzet.maven.plugin;
 
-import java.io.File;
 import java.nio.file.Paths;
 
 import org.apache.maven.execution.MavenSession;
@@ -11,13 +10,11 @@ import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.ProjectBuilder;
-import org.apache.maven.project.ProjectBuildingException;
 import org.apache.maven.settings.Settings;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import com.github.swissquote.carnotzet.core.Carnotzet;
 import com.github.swissquote.carnotzet.core.CarnotzetConfig;
-import com.github.swissquote.carnotzet.core.CarnotzetDefinitionException;
 import com.github.swissquote.carnotzet.core.maven.CarnotzetModuleCoordinates;
 import com.github.swissquote.carnotzet.core.runtime.api.ContainerOrchestrationRuntime;
 import com.github.swissquote.carnotzet.core.runtime.log.LogListener;
@@ -69,7 +66,6 @@ public abstract class AbstractZetMojo extends AbstractMojo {
 
 	@Override
 	public void execute() throws MojoFailureException, MojoExecutionException {
-		MavenProject project = getCarnotzetProject();
 		SLF4JBridgeHandler.install();
 
 		CarnotzetConfig config = CarnotzetConfig.builder()
@@ -86,37 +82,6 @@ public abstract class AbstractZetMojo extends AbstractMojo {
 	}
 
 	public abstract void executeInternal() throws MojoExecutionException, MojoFailureException;
-
-	/**
-	 * @return the currently built project if it's a carnotzet, or a child/sibling otherwise
-	 */
-	public MavenProject getCarnotzetProject() {
-		if (!project.getArtifactId().endsWith("-carnotzet")) {
-			getLog().info("Current project is not a valid carnotzet project. ArtifactId must end with '-carnotzet'");
-
-			//try sibling
-			File tryOther = new File(project.getBasedir().getParentFile(), "carnotzet");
-			if (!tryOther.exists()) {
-				//try child
-				tryOther = new File(project.getBasedir(), "carnotzet");
-			}
-
-			if (tryOther.exists()) {
-				getLog().info("Found a potential carnotzet in " + tryOther);
-				try {
-					session.getProjectBuildingRequest().setResolveDependencies(true);
-					MavenProject newProject =
-							projectBuilder.build(new File(tryOther, "pom.xml"), session.getProjectBuildingRequest()).getProject();
-					getLog().info("New Project:" + newProject);
-					return newProject;
-				}
-				catch (ProjectBuildingException e) {
-					throw new CarnotzetDefinitionException(e);
-				}
-			}
-		}
-		return project;
-	}
 
 	public Runnable wrapWithLogFollowIfNeeded(Runnable block) {
 		if (follow) {
